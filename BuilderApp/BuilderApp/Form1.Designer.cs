2a843286ef935346a1a714f55e2ece88b59640bb
Danilo2077 <danilogerasymovsky1@gmail.com>
2024-11-27 21:56:52 +0200

    Add files via upload

---
+++ BuilderApp/BuilderApp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace BuilderApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReportButton = new System.Windows.Forms.Button();
+            this.textBoxReport = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ReportButton
+            // 
+            this.ReportButton.Location = new System.Drawing.Point(316, 337);
+            this.ReportButton.Name = "ReportButton";
+            this.ReportButton.Size = new System.Drawing.Size(109, 23);
+            this.ReportButton.TabIndex = 0;
+            this.ReportButton.Text = "Створити звіт";
+            this.ReportButton.UseVisualStyleBackColor = true;
+            this.ReportButton.Click += new System.EventHandler(this.ReportButton_Click);
+            // 
+            // textBoxReport
+            // 
+            this.textBoxReport.Location = new System.Drawing.Point(60, 12);
+            this.textBoxReport.Multiline = true;
+            this.textBoxReport.Name = "textBoxReport";
+            this.textBoxReport.ReadOnly = true;
+            this.textBoxReport.Size = new System.Drawing.Size(693, 309);
+            this.textBoxReport.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(339, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxReport);
+            this.Controls.Add(this.ReportButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReportButton;
+        private System.Windows.Forms.TextBox textBoxReport;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BuilderApp/BuilderApp/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BuilderLibrary;
+
+namespace BuilderApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReportButton_Click(object sender, EventArgs e)
+        {
+            // Використовуємо Builder для створення звіту
+            var report = new ReportBuilder()
+                .SetTitle("Звіт про виконану роботу")   // Встановлення заголовка
+                .SetContent("У цьому розділі наводяться основні дані...") // Встановлення змісту
+                .SetSummary("Звіт завершено успішно.")   // Встановлення підсумку
+                .Build();  // Будуємо звіт
+
+            // Виводимо звіт у TextBox
+            textBoxReport.Text = $"Title: {report.Title}\nContent: {report.Content}\nSummary: {report.Summary}";
+        }
+    }
+}
